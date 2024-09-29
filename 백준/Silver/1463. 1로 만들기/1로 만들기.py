92e8837716884ce7179061f9a91e83a402e38766
chlwoomin <72542430+chlwoomin@users.noreply.github.com>
2024-09-29 18:46:42 +0900

    [Silver III] Title: 1로 만들기, Time: 400 ms, Memory: 38932 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -0,0 +1,16 @@
+#1463
+n = int(input())
+dp = [0] * (10**6+1)
+dp[2] = 1
+dp[3] = 1
+for i in range(4, n+1):
+    if i % 3 == 0:
+        if i % 2 == 0:
+            dp[i] = min(dp[i//3], dp[i//2], dp[i-1]) + 1
+        else:
+            dp[i] = min(dp[i//3], dp[i-1]) + 1
+    elif i % 2 == 0:
+        dp[i] = min(dp[i//2], dp[i-1]) + 1
+    else:
+        dp[i] = dp[i-1] + 1
+print(dp[n])
