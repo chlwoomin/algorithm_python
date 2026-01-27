640ff0a1f5bb7fd0aa6f3fddfb8a1406041d9e3e
chlwoomin <72542430+chlwoomin@users.noreply.github.com>
2026-01-27 10:20:30 +0900

    [Silver III] Title: 피보나치 함수, Time: 28 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -0,0 +1,9 @@
+t = int(input())
+n = [int(input()) for _ in range(t)]
+dp = [0,0] * 40
+dp[0] = [1,0]
+dp[1] = [0,1]
+for i in range(2, max(n)+1):
+    dp[i] = [dp[i-1][0] + dp[i-2][0], dp[i-1][1] + dp[i-2][1]]
+for i in n:
+    print(dp[i][0], dp[i][1])
